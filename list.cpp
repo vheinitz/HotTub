a07ef481fe90500bb52ed7cebfe35e97107144cf
Matt Jennings <jennmat@gmail.com>
2011-03-19 15:52:57 -0600

    Bug fix

--- list.cpp
+++ list.cpp
@@ -10,7 +10,10 @@ List::List(QWidget *parent) : QTableView(parent) {
 
 void List::selectionChanged ( const QItemSelection &selected, const QItemSelection &deselected ) {
     Q_UNUSED(deselected);
-    int index = selected.indexes().first().row();
-    emit selectionChanged(index);
+    if ( !selected.indexes().isEmpty() ) {
+        int index = selected.indexes().first().row();
+        emit selectionChanged(index);
+    }
+    
 } 
 
